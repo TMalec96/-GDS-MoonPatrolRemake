256dd2527a304f302eaa3858f70b14540ad2ff82
TMalec96 <tomasz.malecki1996@gmail.com>
2019-11-13 12:36:30 +0100

    First prototype
    Implemented mechanics of jumping and const. moving left

---
+++ Vice Patrol/KinematicBody2D.gd
@@ -0,0 +1,17 @@
+extends KinematicBody2D
+const UP = Vector2(0,-1)
+var motion = Vector2()
+func _physics_process(delta):
+	motion.y+=10
+	
+	if Input.is_action_pressed("ui_right"):
+		motion.x = 100
+	if is_on_floor():
+		if Input.is_action_pressed("ui_up"):
+			motion.y = -400
+	
+	
+	move_and_slide(motion,UP)
+	pass
+	
+
