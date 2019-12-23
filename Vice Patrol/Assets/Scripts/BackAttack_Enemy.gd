c7daf3c3475731c5920cb9e772db8f4bf7884dba
TMalec96 <tomasz.malecki1996@gmail.com>
2019-12-23 20:18:40 +0100

    Back enemy fixed

--- Vice Patrol/Assets/Scripts/BackAttack_Enemy.gd
+++ Vice Patrol/Assets/Scripts/BackAttack_Enemy.gd
@@ -8,21 +8,23 @@ var is_dead = false
 export (int) var scoreValue = 100
 export (float) var enemy_boost_time_delay = 1
 export (float) var boost_duration_time = 1
+export (float) var boost_speed_value = 200
 var is_jumped = false
 onready var collision_detector = get_node("Collision_detector")
 
-
+func _ready():
+	start_chase()
 func start_chase():
 	velocity.x = GlobalVariables.playerVelocity_x
 	boost_delay_time = enemy_boost_time_delay
 	yield(get_tree().create_timer(boost_delay_time),"timeout")
-	velocity.x = max_speed + 200
+	velocity.x += boost_speed_value
 	yield(get_tree().create_timer(boost_duration_time),"timeout")
 	velocity.x = GlobalVariables.playerVelocity_x
 	
 	
 func _physics_process(delta):
-	start_chase()
+	
 	velocity = move_and_slide(velocity, Vector2(0, -1),5,4,rad2deg(75))
 	if collision_detector.is_colliding():
 		collision_detector.get_collider().process_damage_enemy()
