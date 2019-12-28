ad6209716293c4dfc1f59c9f6c4a9d16211df97d
TMalec96 <tomasz.malecki1996@gmail.com>
2019-12-28 13:16:33 +0100

    Enemy pathing 2.0

--- Vice Patrol/Assets/Scripts/EnemyPathing.gd
+++ Vice Patrol/Assets/Scripts/EnemyPathing.gd
@@ -1,16 +1,21 @@
 extends Path2D
 
 onready var follow = get_node("PathFollow2D")
-var path_other = preload("res://Curves/Flying_Enemy_Above_Income.tres")
+var path_2= preload("res://Curves/Infinity_Curve2.tres")
+var path_1 = preload("res://Curves/Infinity_Curve.tres")
+var path_3= preload("res://Curves/Infinity_Curve3.tres")
+var path_4= preload("res://Curves/Infinity_Curve4.tres")
 func _ready():
+	var path_array  = [path_1,path_2,path_3,path_4]
 	set_process(true)
+	var index = randi()%3
+	print(index)
+	curve = path_array[index]
+	follow.loop = true
 func _process(delta):
-		follow.set_offset(follow.get_offset() + 350 *delta)
-#		if(follow.offset>1730):
-#			print("kaszanka")
-#			curve = path_other
-#			follow.loop = true
-		
+	follow.set_offset(follow.get_offset() + 200 *delta)
+	
+
 	#spaw CURVE 2D Resource
 
 
