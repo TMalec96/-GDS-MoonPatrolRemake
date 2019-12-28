74903624dfa7d4a5f13e8203038730e5586722c6
TMalec96 <tomasz.malecki1996@gmail.com>
2019-12-28 20:11:31 +0100

    bombing

--- Vice Patrol/Assets/Scripts/BulletEnemy.gd
+++ Vice Patrol/Assets/Scripts/BulletEnemy.gd
@@ -7,15 +7,19 @@ onready var sprite = get_node("Sprite")
 onready var collider = get_node("CollisionShape2D")
 var animation = preload ("res://Scenes/EnemyAboveExplosionBulletAnimation.tscn")
 var was_exploded = false
+var explosion_position
+var animationInstance = null
 func _ready():
 	if !direction_down:
 		apply_impulse(Vector2(),Vector2(-projectile_speed,0).rotated(rotation))
 	else:
 		var xvelocity = GlobalVariables.playerVelocity_x+randi()%51-100
 		apply_impulse(Vector2(),Vector2(xvelocity,projectile_speed).rotated(rotation))
-	var animationInstance = animation.instance()
+	animationInstance = animation.instance()
 	animationInstance.position = get_position()
 	add_child(animationInstance)
+	animationInstance.visible = false
+	animationInstance.playing = false
 	SelfDestruct()
 
 func SelfDestruct():
@@ -29,4 +33,10 @@ func dead():
 func _on_BulletEnemy_FlyingType1_body_entered(body):
 	if "Player" in body.name:
 		body.process_damage_enemy()
+	animationInstance.visible = true
+	animationInstance.playing = true
+	contact_monitor = false
+	collider.disabled = true
+	
+	
 #	queue_free()
