a56f3b8eb246036db7cab61582a62f2a3657bd42
TMalec96 <tomasz.malecki1996@gmail.com>
2020-01-02 21:42:09 +0100

    world loading

--- Vice Patrol/Assets/Scripts/StartScene.gd
+++ Vice Patrol/Assets/Scripts/StartScene.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 
 func _on_StartGameButton_pressed():
-	SceneLoader.goto_scene("res://Scenes/WorldTest.tscn")
+	SceneLoader.goto_scene("res://Scenes/World.tscn")
 	GlobalVariables.hiScore = GlobalVariables.playerScore
 	GlobalVariables.playerScore  = 0
 
