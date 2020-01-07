23e174c9d80fefbc4488341e9d2bf8d11d853f27
TMalec96 <tomasz.malecki1996@gmail.com>
2020-01-07 13:07:14 +0100

    Rocks/Tank explosion

--- Vice Patrol/Assets/Scripts/Enemy.gd
+++ Vice Patrol/Assets/Scripts/Enemy.gd
@@ -2,13 +2,27 @@ extends StaticBody2D
 
 export (int) var scoreValue = 100
 export (bool) var is_destroyable = false
+export (float) var animation_duration = 0.5
 var is_dead = false
 var is_jumped = false
-func dead():
+var animation = preload ("res://Scenes/EnemyAboveExplosionBulletAnimation.tscn")
+var animationInstance = null
+func _ready():
 	if is_destroyable:
+		animationInstance = animation.instance()
+		add_child(animationInstance)
+		animationInstance.visible = false
+		animationInstance.playing = false
+func dead():
+	set_collision_mask_bit(0,false)
+	if is_destroyable:
+		hide()
+		animationInstance.visible = true
+		animationInstance.playing = true
 		set_collision_layer_bit(9,false)
 		is_dead = true
 		GlobalVariables.playerScore += scoreValue
+		yield(get_tree().create_timer(animation_duration),"timeout")
 		queue_free()
 
 func countScore():
@@ -17,7 +31,9 @@ func countScore():
 		is_jumped = true
 func hide():
 	$Sprite.visible = false
-	set_collision_mask_bit(1,false)
+	set_collision_layer_bit(4,false)
+	set_collision_mask_bit(4,false)
+	$CollisionShape2D.disabled = true
 func show():
 	$Sprite.visible = true
-	set_collision_mask_bit(1,true)
+	set_collision_mask_bit(0,true)
